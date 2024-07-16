8d9c4841ec6a40c0b8b26bda81577e03e3d1e16b
Lambdair <lambdair1984@protonmail.com>
2024-07-17 01:48:25 +0900

    add ime

--- configuration.nix
+++ configuration.nix
@@ -50,6 +50,11 @@
     LC_TIME = "ja_JP.UTF-8";
   };
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = [ pkgs.fcitx5-mozc ];
+  };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
