1cb524af7b86eb0a9373932d59992c53a8b76d36
Lambdair <lambdair1984@protonmail.com>
2024-07-19 02:43:56 +0900

    feat: set devShell and formatter

--- flake.nix
+++ flake.nix
@@ -15,31 +15,63 @@
     };
   };
 
-  outputs = inputs: {
-    nixosConfigurations = {
-      myNixOS = inputs.nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
-      };
-    };
-    homeConfigurations = {
-      myHome = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs {
+  outputs =
+    inputs:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = inputs.nixpkgs.lib.genAttrs systems;
+    in
+    {
+      nixosConfigurations = {
+        myNixOS = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          config.allowUnfree = true;
-          overlays = [
-            inputs.rust-overlay.overlays.default
-            inputs.emacs-overlay.overlay
+          modules = [ ./configuration.nix ];
+        };
+      };
+
+      homeConfigurations = {
+        myHome = inputs.home-manager.lib.homeManagerConfiguration {
+          pkgs = import inputs.nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.emacs-overlay.overlay
+            ];
+          };
+          extraSpecialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./home.nix
+            inputs.catppuccin.homeManagerModules.catppuccin
           ];
         };
-        extraSpecialArgs = {
-          inherit inputs;
-        };
-        modules = [
-          ./home.nix
-          inputs.catppuccin.homeManagerModules.catppuccin
-        ];
       };
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              helix
+              nil
+              yazi
+              lazygit
+              delta
+              git
+              jujutsu
+            ];
+            shellHook = ''
+              exec ${pkgs.nushell}/bin/nu
+            '';
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: inputs.nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
     };
-  };
 }
