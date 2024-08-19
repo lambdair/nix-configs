d31712c6e270310bd786e9235c2b37cd478388a6
Lambdair <lambdair1984@protonmail.com>
2024-08-19 16:52:40 +0000

    feat[nvim]: setting cmp

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -45,7 +45,27 @@
         };
       };
 
-      cmp.enable = true;
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings = {
+          mapping = {
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<C-Space>" = "cmp.mapping.complete()";
+          };
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "latex_symbols"; }
+          ];
+        };
+      };
+      cmp-nvim-lsp.enable = true;
+      cmp-latex-symbols.enable = true;
 
       mini.enable = true;
       which-key = {
