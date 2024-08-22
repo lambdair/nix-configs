cff9bed693954c1f17601bed0fd9b9f43d90d974
Lambdair <lambdair1984@protonmail.com>
2024-08-22 17:41:19 +0900

    feat[nvim]: add commands for lsp and git

--- home/nvim/keymaps.nix
+++ home/nvim/keymaps.nix
@@ -26,4 +26,50 @@
     action = "<cmd>Neotree toggle<CR>";
     key = "<C-w>e";
   }
+
+  # LSP
+  {
+    action = "<cmd>Telescope lsp_definitions<CR>";
+    key = "gd";
+  }
+  {
+    action = "<cmd>Telescope lsp_references<CR>";
+    key = "gr";
+  }
+  {
+    action = "<cmd>Telescope lsp_workspace_symbols<CR>";
+    key = "<leader>s";
+  }
+  {
+    action = "<cmd>lua vim.lsp.buf.hover()<CR>";
+    key = "<leader>k";
+  }
+  {
+    action = "<cmd>lua vim.lsp.buf.rename()<CR>";
+    key = "<leader>r";
+  }
+
+  # LazyGit
+  {
+    action = "<cmd>LazyGit<CR>";
+    key = "<leader>gg";
+  }
+
+  # Gitsigns
+  {
+    action = "<cmd>Gitsigns prev_hunk<CR>";
+    key = "[g";
+  }
+  {
+    action = "<cmd>Gitsigns next_hunk<CR>";
+    key = "]g";
+  }
+  {
+    action = "<cmd>Gitsigns preview_hunk<CR>";
+    key = "<leader>gv";
+  }
+  {
+    action = "<cmd>Gitsigns reset_hunk<CR>";
+    key = "<leader>gr";
+  }
 ]
