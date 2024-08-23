dec3323884a624be9d08d2f80b5117ae2a350056
Lambdair <lambdair1984@protonmail.com>
2024-08-23 09:03:07 +0900

    chore: delete commented out codes

--- home/default.nix
+++ home/default.nix
@@ -1,8 +1,6 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-rec {
-  # home.username = "lambdair";
-  # home.homeDirectory = "/home/${home.username}";
+{
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
@@ -24,17 +22,6 @@ rec {
 
     # gui tools
     emacs-git
-    # vivaldi
-    # thunderbird
-    # discord
-    # (pkgs.appimageTools.wrapType2 {
-    #   name = "heptabase";
-    #   version = "1.34.0";
-    #   src = fetchurl {
-    #     url = "https://github.com/heptameta/project-meta/releases/download/v1.34.0/Heptabase-1.34.0.AppImage";
-    #     sha256 = "0lbmyb3wr4jj0w47b3zdxlrnq8dyb5yjqzr4lmqqrhvxbabs037b";
-    #   };
-    # })
   ];
 
   imports = [
