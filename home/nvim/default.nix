3550ddaf85b6cd7c556956a126d4f83b1c927b46
Lambdair <lambdair1984@protonmail.com>
2024-09-24 16:47:59 +0000

    feat[nvim]: use arrow key to select item of completion

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -68,7 +68,9 @@
         settings = {
           mapping = {
             "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
             "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
             "<CR>" = "cmp.mapping.confirm({ select = true })";
             "<C-f>" = "cmp.mapping.scroll_docs(4)";
             "<C-b>" = "cmp.mapping.scroll_docs(-4)";
