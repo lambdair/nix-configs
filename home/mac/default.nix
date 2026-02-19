597593a6da236f8e58247add0c7e9499f71c74ea
Lambdair <lambdair1984@protonmail.com>
2026-02-19 18:25:08 +0900

    feat: add macskk for macOS (awrit commented out)

--- home/mac/default.nix
+++ home/mac/default.nix
@@ -1,4 +1,22 @@
+{ pkgs, sources, ... }:
+
+# let
+#   awrit = pkgs.stdenv.mkDerivation {
+#     inherit (sources.awrit) pname version src;
+#
+#     installPhase = ''
+#       mkdir -p $out/Applications $out/bin
+#       cp -r lib/awrit/awrit.app $out/Applications/
+#       ln -s $out/Applications/awrit.app/Contents/MacOS/awrit $out/bin/awrit
+#     '';
+#   };
+# in
 rec {
   home.username = "lambdair";
   home.homeDirectory = "/Users/${home.username}";
+
+  home.packages = with pkgs; [
+    macskk # Japanese SKK input method for macOS
+    # awrit # Chromium-based browser for Kitty terminal
+  ];
 }
