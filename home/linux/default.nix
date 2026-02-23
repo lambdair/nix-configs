167d76f00ff8e3740ed6496a028a659e4cc87232
Lambdair <lambdair1984@protonmail.com>
2026-02-23 23:43:12 +0900

    feat(linux): add desktop entry and icon for heptabase

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -1,10 +1,19 @@
 { pkgs, sources, ... }:
 
 let
+  heptabaseContents = pkgs.appimageTools.extractType2 {
+    pname = "heptabase";
+    inherit (sources.heptabase) version src;
+  };
   heptabase = pkgs.appimageTools.wrapType2 {
     pname = "heptabase";
-    version = sources.heptabase.version;
-    src = sources.heptabase.src;
+    inherit (sources.heptabase) version src;
+    extraInstallCommands = ''
+      install -m 444 -D ${heptabaseContents}/project-meta.desktop $out/share/applications/heptabase.desktop
+      install -m 444 -D ${heptabaseContents}/usr/share/icons/hicolor/0x0/apps/project-meta.png $out/share/icons/hicolor/512x512/apps/heptabase.png
+      substituteInPlace $out/share/applications/heptabase.desktop \
+        --replace-fail 'Exec=AppRun' 'Exec=heptabase'
+    '';
   };
 in
 rec {
