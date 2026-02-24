8d69ee5b6a1b4222b7830750ac065476fb250495
Lambdair <lambdair1984@protonmail.com>
2026-02-24 16:32:19 +0900

    fix: explicitly set nix.package

--- host/base.nix
+++ host/base.nix
@@ -2,6 +2,7 @@
 
 {
   nix = {
+    package = pkgs.nix;
     settings = {
       experimental-features = [
         "nix-command"
--- host/sonora/default.nix
+++ host/sonora/default.nix
@@ -1,6 +1,7 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
+  nix.package = pkgs.nix;
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = [
