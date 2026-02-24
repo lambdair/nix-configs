eff4e48e2f3349e8a30ac236ce75f7eb9927c87a
Lambdair <lambdair1984@protonmail.com>
2026-02-25 02:52:40 +0900

    feat(daiquiri): enable Bluetooth with blueman

--- host/daiquiri/default.nix
+++ host/daiquiri/default.nix
@@ -98,6 +98,11 @@
     #media-session.enable = true;
   };
 
+  # Enable Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
