45dac62411835eace8370ee77bb98f530f630c9c
Lambdair <lambdair1984@protonmail.com>
2026-02-24 23:36:37 +0900

    fix(daiquiri): wrap niri-session in login shell for greetd

--- host/daiquiri/default.nix
+++ host/daiquiri/default.nix
@@ -61,7 +61,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd \"bash -l -c niri-session\"";
         user = "greeter";
       };
     };
