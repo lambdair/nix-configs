ae7da7cb154f396e56ba56be05514675c0371510
Lambdair <lambdair1984@protonmail.com>
2026-03-30 16:14:08 +0900

    refactor(host): extract shared cache settings into host/cache.nix

--- host/sonora/default.nix
+++ host/sonora/default.nix
@@ -10,22 +10,10 @@
   # Match existing Nix installation's nixbld group GID.
   ids.gids.nixbld = 30000;
 
+  imports = [ ../cache.nix ];
+
   # Necessary for using flakes on this system.
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://devenv.cachix.org"
-      "https://lambdair.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "lambdair.cachix.org-1:54ySwbQBraOrS9rspPG7Ir31xvKwLBzY3rv7ovkvBIA="
-    ];
-  };
+  nix.settings.experimental-features = "nix-command flakes";
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
