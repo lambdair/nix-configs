a16fe6c945973694d09ef0bd88c0c25bc2a221bf
Lambdair <lambdair1984@protonmail.com>
2026-05-14 14:32:46 +0900

    feat(home/nushell): add cl alias for claude

--- home/default.nix
+++ home/default.nix
@@ -292,6 +292,7 @@ in
         ek = "emacsclient -e '(kill-emacs)'";
         vi = "nvim";
         uu = "uiua repl";
+        cl = "claude";
         nrepl = "clj -Sdeps '{:deps {cider/cider-nrepl {:mvn/version \"0.52.0\"} }}' -m nrepl.cmdline --middleware \"[cider.nrepl/cider-middleware]\"";
       };
       environmentVariables = {
