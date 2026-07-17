0852f187e4b7a674dfad0f58a3d7041606630cfa
Lambdair <lambdair1984@protonmail.com>
2026-07-17 11:44:54 +0900

    fix(difit): migrate pnpm deps to fetcherVersion 4

--- home/difit.nix
+++ home/difit.nix
@@ -24,8 +24,8 @@ let
 
     pnpmDeps = fetchPnpmDeps {
       inherit (finalAttrs) pname version src;
-      fetcherVersion = 3;
-      hash = "sha256-4gsCqgRI2KCMoTP8WCiOaTHOFnbIqC5js8Vk0/6lcWo=";
+      fetcherVersion = 4;
+      hash = "sha256-usFNABztdR24zn+CQ/w8CnpFpY7DQEDrNmNzmGhZgD4=";
     };
 
     buildPhase = ''
