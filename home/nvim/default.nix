000d0223f181c2f17b5e053cae78552aaade8aa5
Lambdair <lambdair1984@protonmail.com>
2026-07-27 10:21:23 +0900

    fix(nvim): build prolog treesitter parser from grammar (dropped from nvim-treesitter-parsers)

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -68,7 +68,13 @@ in
       nvim-treesitter
       nvim-treesitter-parsers.racket
       nvim-treesitter-parsers.fennel
-      nvim-treesitter-parsers.prolog
+      # nvim-treesitter-parsers has no prolog parser, so build it from the
+      # tree-sitter grammar (parser + its bundled queries).
+      (pkgs.runCommandLocal "vimplugin-treesitter-parser-prolog" { } ''
+        mkdir -p "$out/parser" "$out/queries/prolog"
+        cp ${pkgs.tree-sitter.builtGrammars.tree-sitter-prolog}/parser "$out/parser/prolog.so"
+        cp ${pkgs.tree-sitter.builtGrammars.tree-sitter-prolog}/queries/*.scm "$out/queries/prolog/"
+      '')
       lean-nvim
       parinfer-rust
       vim-racket
